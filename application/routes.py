7cb06199309af406875d3ddc5d21ff39c64377d6
Jonas-Luetolf <jonas.luetolf@outlook.com>
2023-02-13 22:40:12 +0100

    update routes

--- application/routes.py
+++ application/routes.py
@@ -1,1 +1,24 @@
+from flask import Blueprint, render_template
 
+routes = Blueprint("routes", __name__)
+
+
+@routes.route("/", methods = ["GET"])
+def index():
+    """
+    renders the index page
+    
+    :return: rendered template
+    """
+
+    return render_template("index.html")
+
+@routes.route("/overview", methods = ["GET"])
+def overview():
+    """
+    renders the overview template and inserts a list of all trains
+    
+    :route: rendered template
+    """
+
+    raise NotImplemented
