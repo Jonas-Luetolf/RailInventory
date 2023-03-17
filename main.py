f62e43e51f2b799cee35581ccebc3592f695a150
Jonas Lütolf <jonas-luetolf@outlook.com>
2023-03-17 17:24:04 +0100

    add app as global var

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from application import create_app
 from config import Config
 
+app = create_app()
+
 
 def main():
     """
     entry point for RailInventory
     """
-
+    
     app = create_app()
     app.run(host="localhost")
 
