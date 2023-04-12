2ca6254e8a11866bd1199deeb3ffd8884efffafc
Jonas-Luetolf <jonas.luetolf@outlook.com>
2023-04-12 23:31:54 +0200

    clean up railinventory.sh

--- railinventory.sh
+++ railinventory.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 
-
 case $1 in
-	"run") docker run --restart=always -p 443:443 -d --mount type=bind,source=/var/RailInventory,target=/app/var -it ghcr.io/jonas-luetolf/railinventory:main;;
+	"run") 
+        docker run --restart=always -p 443:443 -d --mount type=bind,source=/var/RailInventory,target=/app/var -it ghcr.io/jonas-luetolf/railinventory:main;;
 	
 	"init") 
         docker pull ghcr.io/jonas-luetolf/railinventory:main
+        
         mkdir /var/RailInventory
 		openssl req -newkey rsa:2048 -nodes -keyout /var/RailInventory/key.pem -x509 -days 365 -out /var/RailInventory/certificate.pem
 		wget -O /var/RailInventory/.env https://github.com/Jonas-Luetolf/RailInventory/releases/download/v0.0.1/default.env;;	
-	esac
 
     "update")
         docker kill ghcr.io/jonas-luetolf/railinventory:main
         docker pull ghcr.io/jonas-luetolf/railinventory:main
         docker run --restart=always -p 443:443 -d --mount type=bind,source=/var/RailInventory,target=/app/var -it ghcr.io/jonas-luetolf/railinventory:main;;
+
+    esac
