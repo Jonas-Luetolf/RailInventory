2981e9847b474d85bd05ac30353a0a05c9ca06c7
Jonas Lütolf <jonas.luetolf@outlook.com>
2023-10-03 16:00:51 +0200

    fix BUG: order of data in index page
    - send the data via form
    - render the data via dict with all wanted fields

--- application/views.py
+++ application/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, url_for, current_app
+
+from application.helpers import list_to_form_list
 from .database import DataBase, TrainType
 
 views = Blueprint("views", __name__, url_prefix="/", template_folder="templates")
@@ -14,4 +16,6 @@ def index():
     db = DataBase(current_app.config["DATABASE"])
     locomotives = db.get_all_trains(train_type=TrainType.LOCOMOTIVE)
     wagons = db.get_all_trains(train_type=TrainType.WAGON)
+    locomotives = list_to_form_list(locomotives, TrainType.LOCOMOTIVE)
+    wagons = list_to_form_list(wagons, TrainType.WAGON)
     return render_template("index.html", locomotives=locomotives, wagons=wagons)
