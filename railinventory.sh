00e47b1896c4c9d86cae2867ad1cc16de258890b
Jonas Lütolf <jonas.luetolf@outlook.com>
2023-11-23 17:59:38 +0100

    Update railinventory.sh

--- railinventory.sh
+++ railinventory.sh
@@ -3,7 +3,7 @@
 
 case $1 in
 	"run") 
-        docker run --restart=always -p 443:443 -d --mount type=bind,source=/var/RailInventory,target=/app/var -it ghcr.io/jonas-luetolf/railinventory:main;;
+        docker run --restart=always -p 80:80 -d --mount type=bind,source=/var/RailInventory,target=/app/var -it ghcr.io/jonas-luetolf/railinventory:main;;
 	
 	"init") 
         docker pull ghcr.io/jonas-luetolf/railinventory:main
